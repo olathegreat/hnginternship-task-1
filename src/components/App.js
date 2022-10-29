c63518dc7dec074d23aad8e924437678739c32ff
olathegreat <ademolarotimi2000@gmail.com>
2022-10-29 20:13:25 +0100

    i added accordion effect to the subtext

--- src/components/App.js
+++ src/components/App.js
@@ -16,11 +16,27 @@ const App = () => {
 
       <ul>
          <li><a  href=''>Twitter Link</a></li>
-         <li><a id="btn_zuri" href='https://training.zuri.team'>Zuri Team</a></li>
-         <li><a id="books" href='http://books.zuri.team'>Zuri Books</a></li>
-         <li><a id='book_python' href='https://books.zuri.com/python-for-beginners?ref_id=<yourslackname>'>Python Books</a></li>
-         <li><a id='pitch' href='https://background.zuri.team'>Background Check for Coders</a></li>
-         <li><a id="book_design" href='https://books.zuri.team/design-rules'>Design Books</a></li>
+         <li>
+              <a id="btn_zuri" href='https://training.zuri.team'>Zuri Team</a>
+
+             <sub>Join the zuri team</sub>
+          </li>
+         <li>
+          <a id="books" href='http://books.zuri.team'>Zuri Books</a>
+          <sub>This is where you find books about design and coding</sub>
+          </li>
+         <li>
+          <a id='book_python' href='https://books.zuri.com/python-for-beginners?ref_id=<yourslackname>'>Python Books</a>
+          <sub>Find books to help you learn python from the best</sub>
+          </li>
+         <li>
+          <a id='pitch' href='https://background.zuri.team'>Background Check for Coders</a>
+          <sub>We help you know more about your coder's background</sub>
+          </li>
+         <li>
+          <a id="book_design" href='https://books.zuri.team/design-rules'>Design Books</a>
+          <sub>where you pitch the free design book offered by Zuri.</sub>
+          </li>
 
 
 
